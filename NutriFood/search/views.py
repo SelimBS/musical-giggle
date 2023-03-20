722050e89c488f9ed9f39a7494aa790129aa157c
SemilZola <selim.benslama@cri-paris.org>
2023-03-20 21:23:15 +0100

    Premiere Vue Django

---
+++ NutriFood/search/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hey ! tu es sur l'index de l'application search du projet NutriFood.")
+
+# Create your views here.
